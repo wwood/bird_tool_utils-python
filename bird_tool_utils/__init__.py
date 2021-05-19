b79c80b8c2deb10fbf14df4865128b55ad1d9fbd
Ben Woodcroft <benjwoodcroft@gmail.com>
2021-05-19 21:02:33 +1000

    Add in_tempdir, in_working_directory contexts.

--- bird_tool_utils/__init__.py
+++ bird_tool_utils/__init__.py
@@ -1,5 +1,27 @@
+import os
+import contextlib
+from pathlib import Path
+import tempfile
+
 from .argparsing import *
 
 
+@contextlib.contextmanager
+def in_working_directory(path):
+    """Changes working directory and returns to previous on exit."""
+    prev_cwd = Path.cwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(prev_cwd)
 
 
+@contextlib.contextmanager
+def in_tempdir():
+    '''Create a new temporary directory and chdir there as a context i.e. chdir
+    back when finished'''
+
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        with in_working_directory(tmpdirname):
+            yield
