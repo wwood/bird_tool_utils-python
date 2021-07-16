9fc8b11d233173fb9fb277529277d65117c983e1
Mitchell Cunningham <mitchac@gmail.com>
2021-07-16 14:47:28 +1000

    tidy yaml file, increment version

--- cloudbuild/release_pypi.sh
+++
@@ -1,5 +0,0 @@
-echo $(gcloud secrets versions access latest --secret="wwood-bird-tool-utils-pypi-api-token" --project="maximal-dynamo-308105")
-
-python3 -m pip install --upgrade build setuptools twine wheel
-python3 -m build
-python3 -m twine upload -u __token__ -p $(gcloud secrets versions access latest --secret="wwood-bird-tool-utils-pypi-api-token" --project="maximal-dynamo-308105") dist/*
