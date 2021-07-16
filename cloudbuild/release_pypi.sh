f4cdb3d71ffd62c04af7307bae9a379e619b2e5b
Mitchell Cunningham <mitchac@gmail.com>
2021-07-16 12:39:42 +1000

    set up cloudbuild

---
+++ cloudbuild/release_pypi.sh
@@ -0,0 +1,8 @@
+if [ "${TWINE_PASSWORD}" == "" ]
+then
+    TWINE_PASSWORD=$(gcloud secrets versions access latest --secret="wwood-bird-tool-utils-pypi-api-token" --project=${PROJECT_ID})
+fi
+
+python3 -m pip install --upgrade build setuptools twine wheel
+python3 -m build
+python3 -m twine upload dist/*<Paste>
